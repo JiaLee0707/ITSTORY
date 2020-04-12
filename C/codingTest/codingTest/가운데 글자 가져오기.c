e47152c673ec25a7bb73cf24b6460ca2a8df5c52
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-04-12 23:31:50 +0900

    Level 1 : 첫번째 문제

---
+++ C/codingTest/codingTest/가운데 글자 가져오기.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* solution(const char* s) {
+
+    int size = (int) strlen(s);
+
+    char *answer = (char*)malloc(2);
+    memset(answer, 0, sizeof(int)*2);
+
+    if (size % 2 == 0) {
+        *answer = s[size / 2 - 1];
+        *(answer+1) = s[size / 2];
+    }
+    else {
+        *answer = s[size / 2];
+    }
+
+
+    return answer;
+    free(answer);
+}
+
+int main(void) {
+    char s[100];
+    printf("문자열을 입력하세요 : ");
+    scanf("%s", s);
+
+    //solution(s);
+
+    printf("%s", solution(s));
+}
